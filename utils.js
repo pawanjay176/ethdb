198ee9ea2fe028a2104f39760648cb4803c42792
pawanjay176 <pawandhananjay@gmail.com>
2018-10-06 10:41:46 +0530

    Table schema

--- utils.js
+++ utils.js
@@ -24,7 +24,40 @@ export async function initializeTables () {
     value varchar(34) not null,
     gasPrice varchar(32) not null,
     gas INT(32) not null,
-    input text not null
+    input text not null,
+    status boolean,
+)`;
+
+let createReceipts = `create table if not exists receipts(
+  
+  blockHash char(66) not null,
+  blockNumber INT(64) not null,
+  transactionHash char(66) primary key not null,
+  transactionIndex INT(64) not null
+  fromAdd char(42) not null,
+  toAdd char(42),
+  contractAddress char(42),
+  cumulativeGasUsed INT(64),
+  gasUsed INT(64),
+)`;
+
+let createBlocks = `create table if not exists blocks(
+  hash char(66) primary key,
+  number INT(64) not null,
+  parentHash char(66) not null,
+  nonce char(18) not null,
+  sha3Uncles char(66) not null,
+  logsBloom text not null,
+  transactionsRoot char(66) not null,
+  stateRoot char(66) not null,
+  miner char(42) not null,
+  difficulty text not null,
+  totalDifficulty text not null,
+  extraData text,
+  size INT(64) not null,
+  gasLimit INT(64) not null,
+  gasUsed INT(64) not null,
+  timestamp INT(64) not null,
 )`;
 
   try {
