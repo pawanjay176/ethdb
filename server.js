518e2cca34d791da00f12b073dd138c3bf6cb8d0
pawanjay176 <pawandhananjay@gmail.com>
2018-10-06 13:19:17 +0530

    Write block,transaction and uncles to db

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-import {getWeb3, initializeTables} from "./utils.js";
+import {getWeb3, initializeSchema} from "./utils.js";
 import express from "express";
 var mysql = require('mysql');
 var pool = require('./db')
@@ -6,49 +6,71 @@ const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 let web3;
-let db;
 
+// Return a subscription object
 async function getSubscription () {
   var subscription = await web3.eth.subscribe('newBlockHeaders');
   return subscription;
 }
 
-async function writeTransactionToDb(t) {
-  let sql = `INSERT INTO transactions(transactionHash,nonce,blockHash,blockNumber,transactionIndex,fromAdd,toAdd,value,gasPrice,gas,input)
-           VALUES(?,?,?,?,?,?,?,?,?,?,?)`;
-  let vals = [t.hash, t.nonce, t.blockHash, t.blockNumber, t.transactionIndex, t.from, t.to, t.value, t.gasPrice, t.gas, t.input];
+// Write a transaction with its receipt data into db
+async function writeTransactionToDb(transaction) {
+  let t = await web3.eth.getTransaction(transaction);
+  let r = await web3.eth.getTransactionReceipt(t.hash);
+  let sql = `INSERT INTO transaction(transactionHash,nonce,blockhash,blockNumber,transactionIndex,fromAdd,toAdd,value,gasPrice,gas,input,status,contractAddress,cumulativeGasUsed,gasUsed)
+           VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)`;
+  let vals = [t.hash, t.nonce, t.blockHash, t.blockNumber, t.transactionIndex, t.from, t.to, t.value, t.gasPrice, t.gas, t.input, r.status, r.contractAddress, r.cumulativeGasUsed, r.gasUsed];
   try {
     var res = await pool.query(sql, vals);
   } catch(err) {
     console.log(err);
   }
-
 }
 
-async function getTransaction(t) {
-  let transaction = await web3.eth.getTransaction(t);
-  console.log(transaction);
-  await writeTransactionToDb(transaction);
+// Write uncle hash to db
+async function writeUnclesToDb(uncleHash, blockHash) {
+  let sql = `INSERT INTO uncle(hash,blockhash)
+  VALUES(?,?)`;
+  let vals = [uncleHash, blockHash];
+  try {
+    var res = await pool.query(sql, vals);
+  } catch(err) {
+    console.log(err);
+  }
 }
 
+// Write blocks to db
+async function writeBlockToDb(b) {
+  let sql = `INSERT INTO block(hash,number,parentHash,nonce,sha3Uncles,logsBloom,transactionsRoot,stateRoot,miner,difficulty,totalDifficulty,extraData,blocksize,gasLimit,gasUsed,timestamp)
+  VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)`;
+  let vals = [b.hash, b.number, b.parentHash, b.nonce, b.sha3Uncles, b.logsBloom, b.transactionsRoot, b.stateRoot, b.miner, b.difficulty, b.totalDifficulty, b.extraData, b.size, b.gasLimit, b.gasUsed, b.timestamp];
+  try {
+    var res = await pool.query(sql, vals);
+  } catch(err) {
+    console.log(err);
+  }
+  b.transactions.map(t => writeTransactionToDb(t).then(console.log));
+  b.uncles.map(u => writeUnclesToDb(u, b.hash).then(console.log));
+}
+
+// Init db schema and web3 object
 async function init() {
   web3 = await getWeb3();
-  db = await initializeTables();
+  db = await initializeSchema();
 }
 
-
 // io.set('origins', '*:*');
+// 
 io.on('connection', async (socket) => {
   console.log("Client Successfully Connected");
   await init();
   let subscription = await getSubscription();
-  
   subscription.on("data", async (blockHeader) => {
-      // console.log(blockHeader);
+      // consider getting transaction objects in the same api call
       let block = await web3.eth.getBlock(blockHeader.hash);
-      // console.log(block);
-      block.transactions.map(t => getTransaction(t).then(console.log));
+      console.log(block);
       io.emit('chat', block);
+      await writeBlockToDb(block);
   })
   subscription.on("error", console.error);
 })
--- utils.js
+++ utils.js
@@ -12,56 +12,62 @@ export const getWeb3 = () =>
       
   });
 
-export async function initializeTables () {
-  let createTransactions = `create table if not exists transactions(
-    transactionHash varchar(66) primary key,
+export async function initializeSchema () {
+  // don't see a point of storing the logs indexed by transactions
+  // skipping for now
+  // merging transaction receipt with transaction table 
+  let transactionSql = `create table if not exists transaction(
+    transactionHash char(66) primary key,
     nonce INT(32) not null,
-    blockHash varchar(66) not null,
+    blockhash char(66) not null,
     blockNumber INT(32) not null,
     transactionIndex INT(32) not null,
-    fromAdd varchar(42) not null,
-    toAdd varchar(42),
-    value varchar(34) not null,
+    fromAdd char(42) not null,
+    toAdd char(42),
+    value char(34) not null,
     gasPrice varchar(32) not null,
     gas INT(32) not null,
     input text not null,
     status boolean,
+    contractAddress char(42),
+    cumulativeGasUsed INT(64),
+    gasUsed INT(64),
+    FOREIGN KEY fk_blockhash(blockhash) 
+    REFERENCES block(hash)
+    ON DELETE CASCADE
 )`;
 
-let createReceipts = `create table if not exists receipts(
-  
-  blockHash char(66) not null,
-  blockNumber INT(64) not null,
-  transactionHash char(66) primary key not null,
-  transactionIndex INT(64) not null
-  fromAdd char(42) not null,
-  toAdd char(42),
-  contractAddress char(42),
-  cumulativeGasUsed INT(64),
-  gasUsed INT(64),
+  let blockSql = `create table if not exists block(
+    hash char(66) primary key,
+    number INT(64) not null,
+    parentHash char(66) not null,
+    nonce char(18),
+    sha3Uncles char(66) not null,
+    logsBloom text,
+    transactionsRoot char(66) not null,
+    stateRoot char(66) not null,
+    miner char(42) not null,
+    difficulty text not null,
+    totalDifficulty text not null,
+    extraData text,
+    blocksize INT(64) not null,
+    gasLimit INT(64) not null,
+    gasUsed INT(64) not null,
+    timestamp INT(64) not null
 )`;
-
-let createBlocks = `create table if not exists blocks(
-  hash char(66) primary key,
-  number INT(64) not null,
-  parentHash char(66) not null,
-  nonce char(18) not null,
-  sha3Uncles char(66) not null,
-  logsBloom text not null,
-  transactionsRoot char(66) not null,
-  stateRoot char(66) not null,
-  miner char(42) not null,
-  difficulty text not null,
-  totalDifficulty text not null,
-  extraData text,
-  size INT(64) not null,
-  gasLimit INT(64) not null,
-  gasUsed INT(64) not null,
-  timestamp INT(64) not null,
+   
+let uncleSql = `create table if not exists uncle(
+  hash char(66) not null primary key,
+  blockhash char(66) not null,
+  FOREIGN KEY fk_blockhash(blockhash) 
+    REFERENCES block(hash)
+    ON DELETE CASCADE
 )`;
 
   try {
-    var res = await pool.query(createTransactions);
+    await pool.query(blockSql);
+    await pool.query(transactionSql);
+    await pool.query(uncleSql);
   } catch(err) {
     console.log(err);
   }
